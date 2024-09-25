090ee9d13d8237b1e69c6ee0f34519dc541d5cf7
leo <leomutungi19@gmail.com>
2024-09-25 21:17:56 +0300

    .equals expressions

--- first-java-program/first-tutorial/first-idea-program/SwitchStatements/src/com/leo/Main.java
+++ first-java-program/first-tutorial/first-idea-program/SwitchStatements/src/com/leo/Main.java
@@ -7,5 +7,19 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String fruit = in.next();
+
+        // compares the contents of the string
+        if (fruit.equals("mango")) {
+            System.out.println("King of fruits");
+        }
+
+        if (fruit.equals("apple")) {
+            System.out.println("an apple a day keeps the doctor away");
+        }
+
+//        String a = "Leo";
+//        String b = "Leo";
+
+        System.out.println();
     }
 }
