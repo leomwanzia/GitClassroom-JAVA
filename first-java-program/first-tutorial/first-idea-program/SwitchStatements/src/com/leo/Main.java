e6c5d759623444ce04e6ec2891173a8cc35d2782
leo <leomutungi19@gmail.com>
2024-09-25 19:50:03 +0300

    First Commit for Switch Statements and Nested cases

---
+++ first-java-program/first-tutorial/first-idea-program/SwitchStatements/src/com/leo/Main.java
@@ -0,0 +1,11 @@
+package com.leo;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String fruit = in.next();
+    }
+}
